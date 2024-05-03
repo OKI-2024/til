5067e1bfbe78f6c749d5e39974c59ad53e01db34
OKI-2024 <tetsuokinokun@gmail.com>
2024-05-03 10:55:12 +0900

    問1

---
+++ algorithm/problem2.rb
@@ -0,0 +1,29 @@
+# 問1
+
+n = 1
+10.times do
+ puts n
+ n += 1
+end
+
+# もしくは
+
+array = (1..10)
+
+array.each do |i|
+  puts i
+end
+
+# もしくは
+
+n = 1
+
+while n <= 10 do
+ puts n
+ n += 1
+end
+
+# 問1
+
+
+
