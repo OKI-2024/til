070a6af4bba57f43332c51ade2dd26249ed95c22
OKI-2024 <tetsuokinokun@gmail.com>
2024-05-03 11:47:48 +0900

    Update teamlab.rb

--- algorithm/teamlab.rb
+++ algorithm/teamlab.rb
@@ -1,1 +1,21 @@
-puts "Hello World"
+# 1
+
+num = 1041440400
+divisors = []
+
+(1..Math.sqrt(num)).each do |i|
+  if num % i == 0
+    divisors << i
+    divisors << num / i unless i == num / i
+  end
+end
+
+count = 0
+
+divisors.each do |n|
+  if n >= 10000 && n <= 99999
+    count += 1
+  end
+end
+
+puts count
