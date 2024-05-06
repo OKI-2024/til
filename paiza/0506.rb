de8dc6f1c451788d506ef69f52356ed5657d2747
OKI-2024 <tetsuokinokun@gmail.com>
2024-05-06 17:49:30 +0900

    Update 0506.rb

--- paiza/0506.rb
+++ paiza/0506.rb
@@ -1,8 +1,6 @@
 # https://paiza.jp/career/challenges/710/retry
 
-
 jankens = gets.to_i
-
 
 count = 0
 
@@ -23,3 +21,29 @@ jankens.times do
 end
 
 puts count
+
+
+# https://paiza.jp/challenges/560/show
+
+
+
+n, m = gets.split.map(&:to_i)
+# 1行で複数の入力を受け取る
+# ①"10 20"と入力
+# ②splitによって空白を区切りとして、["10", "20"]という配列に
+# ③配列の各要素に対して&:to_iを実行し、数値に変換→[10, 20]
+
+
+
+sum = 0
+
+( n -1 ).times do
+  
+  a = gets.to_i
+  if a <= m
+    sum += a
+  end
+  
+end
+
+puts sum
