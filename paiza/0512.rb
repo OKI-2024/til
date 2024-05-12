6e1bb66a304e7dce365a9ea7d6165f08907bf0b1
OKI-2024 <tetsuokinokun@gmail.com>
2024-05-12 10:16:46 +0900

    Update 0512.rb

--- paiza/0512.rb
+++ paiza/0512.rb
@@ -1,3 +1,5 @@
+# https://paiza.jp/works/challenges/54/page/result
+
 string = gets
 
 string.gsub!(/A|E|G|I|O|S|Z/, "A" => "4", "E" => "3", "G" => "6", "I" => "1", "O" => "0", "S" => "5", "Z" => "2")
