67e1d250870c823c078af39417ee837c63ea79ef
OKI-2024 <tetsuokinokun@gmail.com>
2024-05-13 09:05:12 +0900

    Update 0513.rb

--- paiza/0513.rb
+++ paiza/0513.rb
@@ -1,8 +1,26 @@
 # https://paiza.jp/challenges/85/show
 puts "当選番号を入力してください"
-win_num = gets.split.map(&:to_i)
+win_num = gets.split.map(&:to_i) #.splitは引数なしで使用した場合、半角スペースで区切り配列に配置する
+puts "くじの購入枚数を入力してください"
+nums = gets.to_i
 
+my_nums = []
 
+nums.times do |i|
+  puts "#{i + 1}枚目のくじの番号を入力してください"
+  my_num = gets.split.map(&:to_i)
+  my_nums << my_num
+end
+
+my_nums.each do |i|
+  count = 0
+  win_num.each do |a|
+    if i.include?(a)
+      count += 1
+    end
+  end
+  puts count
+end
 
 
 
