722636ecdfe4de78db7c8eb67454a4c24b8d012c
OKI-2024 <tetsuokinokun@gmail.com>
2024-05-20 10:54:41 +0900

    Update 0520.rb

--- paiza/0520.rb
+++ paiza/0520.rb
@@ -8,12 +8,13 @@ field = []
 
 line.times do |i|
   puts "#{i + 1}行目に設置する爆弾を入力してください（例）設置する；#, 設置しない：."
-  field << gets.chomp.chars
+  field << gets.chomp.chars # charsによって[".", "#", "."]のような配列になる
 end
 
 
 explosion_map = Array.new(line) { Array.new(record, false) }
-
+# Array.new(line)によって[nil, nil, nil]という配列を作成※line = 3の場合
+# Array.new(record, false), lineが3でrecordが4であれば、[false, false, false, false], [false, false, false, false], [false, false, false, false]
 
 field.each_with_index do |row, i|
   row.each_with_index do |cell, j|
@@ -26,6 +27,6 @@ field.each_with_index do |row, i|
 end
 
 
-exploded_count = explosion_map.flatten.count(true)
+exploded_count = explosion_map.flatten.count(true) # flatten メソッドは二次元配列を一次元に変換する
 
 puts exploded_count
